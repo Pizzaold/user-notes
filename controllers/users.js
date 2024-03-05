b316ab2d718f91a070a724ad72ad36fa7a57bc92
Jürmo Mihhailov <jurmoharak@gmail.com>
2024-03-05 10:02:28 +0200

    Create user login

--- controllers/users.js
+++ controllers/users.js
@@ -9,11 +9,9 @@ const register = (req, res) => {
     if (existingUser) {
       return res.json({ message: "Username already exists" });
     }
-
     if (req.body.password.length < 8) {
       return res.json({ message: "Password must be at least 8 characters" });
     }
-
     bcrypt.hash(req.body.password, 10, (err, hash) => {
       User.create({
         username: req.body.username,
@@ -35,4 +33,37 @@ const register = (req, res) => {
   });
 };
 
-module.exports = { register };
+const login = (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({ where: { username: username } })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+          console.error("Error:", err);
+          return res.status(500).json({ message: "Internal server error" });
+        }
+        if (!result) {
+          return res.status(401).json({ message: "Invalid password" });
+        }
+        req.session.user = {
+          username: user.username,
+          user_id: user.id,
+        };
+        console.log(req.session);
+        res.json({
+          message: "Login successful",
+          user: user,
+          user_session: req.session.user,
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      res.status(500).json({ message: "Failed to log in" });
+    });
+};
+
+module.exports = { register, login };
